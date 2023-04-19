e009820ffb27aede36e8213cd72168b393c54e03
JorgeVenegasPe <mariano.venegas.francia@gmail.com>
2023-04-19 12:07:54 -0500

    actualizar pag restaurante

--- PagRestaurante/javascript/main.js
+++ PagRestaurante/javascript/main.js
@@ -1,34 +1,29 @@
-// DOM
-/*let links = document.querySelectorAll("a");
-
-links.forEach(function(link){
-    console.log(link);
-})*/
-
+// Obtener los elementos de la clase .close
 let link = document.querySelectorAll(".close");
 
+// Recorrelos 
 link.forEach(function(links){
+
+    // Agregar un evento click a cada uno de ellos 
     links.addEventListener('click',function(ev){
         ev.preventDefault();
         let content = document.querySelector(".content");
 
+        // Quitarle las clases de animacion que ya tiene 
         content.classList.remove("animate__fadeInDown");
         content.classList.remove("animate__animated");
-    
+
+        // Agregar nuevas clases para animar la salida
         content.classList.add("animate__fadeOutUp");
         content.classList.add("animate__animated");
 
+        // Funcion para el tiempo de salida
         setTimeout(function(){
             location.href = "../index.html";
         },600);
+
         return false;
     })
-})
-
-let iconos = document.querySelectorAll("i");
-
-iconos.forEach(function(icono){
-    icono.classList.remove("fa-star-o");
 })
 
 
